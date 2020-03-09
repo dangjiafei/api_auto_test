e08f3685d86b786071cddcc9541c72621c7ceebf
dangjiafei <18840341320@163.com>
2020-03-09 20:39:14 +0800

    handle log

--- common/handle_log.py
+++ common/handle_log.py
@@ -1,6 +1,7 @@
 # 该文件处理Log在控制台和文件的记录
 
 import os
+import datetime
 import logging
 from common.handle_config import conf
 from common.handle_path import LOG_DIR
@@ -10,6 +11,7 @@ class HandleLog(object):
 
     @staticmethod
     def create_logger():
+        date = datetime.datetime.now().strftime("%m_%d_%H_%M_")
         # 创建收集器，设置收集器的等级
         my_log = logging.getLogger(conf.get("log", "name"))
         my_log.setLevel(conf.get("log", "level"))
@@ -18,7 +20,7 @@ class HandleLog(object):
         sh.setLevel(conf.get("log", "sh_level"))
         my_log.addHandler(sh)
         # 创建输出到文件的渠道，设置等级
-        fh = logging.FileHandler(filename=os.path.join(LOG_DIR, "item_detail.log"), encoding="utf-8")
+        fh = logging.FileHandler(filename=os.path.join(LOG_DIR, date + "log.log"), encoding="utf-8")
         fh.setLevel(conf.get("log", "fh_level"))
         my_log.addHandler(fh)
         # 设置日志输出格式
